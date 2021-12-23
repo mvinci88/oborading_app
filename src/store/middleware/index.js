e95c850d7113c64b699b72503fb816648989ab60
Matteo <90204739+mvinci88@users.noreply.github.com>
2021-12-23 17:14:34 +0100

    Update index.js

--- src/store/middleware/index.js
+++ src/store/middleware/index.js
@@ -4,7 +4,7 @@ import { of } from 'rxjs';
 import { FETCH_START, WEATHER_CHANGE_ERROR } from "../../constants/action_types";
 import { ofType } from 'redux-observable';
 import { fetchSuccess } from '../actions';
-const myAPI = "ce7e5c2a5c0382057cf868ac49f5f516";
+const myAPI = "YOUR_API_KEY";
 
 export const loggerMiddleware = store => next => action => {
   console.log(action);
